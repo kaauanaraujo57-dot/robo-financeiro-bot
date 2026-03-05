303069bd2f5a55af68858b3024bab0005d8d680b
kaauanaraujo57-dot <kaauanaraujo57@gmail.com>
2026-03-05 14:25:59 -0300

    Update bot.py
    import os
    import psycopg2
    from datetime import datetime
    from telegram import Update
    from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
    
    TOKEN = os.getenv("TOKEN")
    DATABASE_URL = os.getenv("DATABASE_URL")
    
    if not DATABASE_URL:
        raise ValueError("DATABASE_URL não encontrada!")
    
    conn = psycopg2.connect(DATABASE_URL)
    cursor = conn.cursor()

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não encontrada!")
+
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
